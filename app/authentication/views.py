835df69a2151e76fccc7ccedc92722dedb5a8d7a
Cyrille YAO <cyaoyapi@gmail.com>
2022-06-08 20:03:40 +0000

    Login page based on generic view django.contrib.auth.views.LoginView

--- app/authentication/views.py
+++ app/authentication/views.py
@@ -1,7 +1,6 @@
 """
 Module to define views of app 'authenication'.
 """
-from email import message
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect, render
@@ -9,37 +8,37 @@ from django.views.generic import View
 
 from authentication.forms import LoginForm
 
-class LoginPageView(View):
-    """View for login page."""
-    form_class = LoginForm
-    template_name = 'authentication/login.html'
+# class LoginPageView(View):
+#     """View for login page."""
+#     form_class = LoginForm
+#     template_name = 'authentication/login.html'
 
-    def get(self, request):
-        """Handle GET request."""
-        message = ''
-        form = self.form_class()
-        return render(request, self.template_name, context={
-            'form': form,
-            'message': message
-        })
+#     def get(self, request):
+#         """Handle GET request."""
+#         message = ''
+#         form = self.form_class()
+#         return render(request, self.template_name, context={
+#             'form': form,
+#             'message': message
+#         })
 
-    def post(self, request):
-        "Handle POST request."
-        message = ""
-        form = self.form_class(request.POST)
-        if form.is_valid():
-            user = authenticate(
-                username=form.cleaned_data['username'], 
-                password=form.cleaned_data['password']
-            )
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-        message = "Identifiants invalides."
-        return render(request, self.template_name, context={
-            'form': form, 
-            'message': message
-        })
+#     def post(self, request):
+#         "Handle POST request."
+#         message = ""
+#         form = self.form_class(request.POST)
+#         if form.is_valid():
+#             user = authenticate(
+#                 username=form.cleaned_data['username'], 
+#                 password=form.cleaned_data['password']
+#             )
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('home')
+#         message = "Identifiants invalides."
+#         return render(request, self.template_name, context={
+#             'form': form, 
+#             'message': message
+#         })
 
 
 # def login_page(request):
