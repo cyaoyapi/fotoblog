a9296375d11d427346b0e3e113503e5c468fe53f
Cyrille YAO <cyaoyapi@gmail.com>
2022-06-08 17:02:36 +0000

    Add package pillow

--- app/authentication/models.py
+++ app/authentication/models.py
@@ -1,3 +1,18 @@
+"""
+Module to define models related customized authentication app.
+"""
+from django.contrib.auth. models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    """Customized User model."""
+
+    class UserRole(models.TextChoices):
+        """User role model."""
+
+        CREATOR = ('Créateur', 'CREATOR')
+        SUBSCRIBER = ('Abonné', 'SUBSCRIBER')
+
+    profile_photo = models.ImageField("Photo de profil")
+    role = models.CharField("Role", max_length=30, choices=UserRole.choices)
+
